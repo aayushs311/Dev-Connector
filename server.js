1f3202f551cd07c0e6a5daab3bdb644cf368f588
Aayush Singh <aayushs311@gmail.com>
2021-04-22 18:27:29 +0530

    JWT implemented

--- server.js
+++ server.js
@@ -3,11 +3,19 @@ const connectDB = require('./config/db')
 
 const app = express();
 
+// Connect database
 connectDB();
 
-app.get('/',(req,res)=>{
-    res.send('API running')
-});
+// Init middleware
+app.use(express.json({extended:false}));
+
+app.get('/',(req,res) => res.send('API running'))
+
+// Define routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/profile', require('./routes/api/profile'));
 
 const PORT = process.env.PORT||3000;
 
